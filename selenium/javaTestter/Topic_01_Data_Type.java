0e6aff19f45ec0cf6144681551b28526cd2c108e
Admin <Admin@DESKTOP-E6N9TK3>
2021-06-09 18:08:11 +0700

    Topic_01_Data_Type

---
+++ selenium/javaTestter/Topic_01_Data_Type.java
@@ -0,0 +1,10 @@
+package javaTestter;
+
+public class Topic_01_Data_Type {
+
+	public static void main(String[] args) {
+	System.out.println("Selenimum Java");
+
+	}
+
+}
---
+++ selenium/webdriver/Topic_02_Xpath_Css_Part_1_Locator.java
@@ -0,0 +1,44 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Xpath_Css_Part_1_Locator {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("");
+	}
+
+	@Test
+	public void TC_01_() {
+		
+	}
+
+	@Test
+	public void TC_02_() {
+		
+	}
+
+	@Test
+	public void TC_03_() {
+		
+	}
+	
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
